dfd67a08ffc25ee238fb3b38a00d20ee5416c863
Nicola Peduzzi <thenikso@gmail.com>
2021-02-27 12:18:18 +0100

    Add event test

--- packages/proma-core/test/index.mjs
+++ packages/proma-core/test/index.mjs
@@ -12,3 +12,4 @@ import './programs/outputs.test.mjs';
 import './programs/connections.test.mjs';
 import './programs/variadic.test.mjs';
 import './programs/handles.test.mjs';
+import './programs/events.test.mjs';
---
+++ packages/proma-core/test/programs/events.test.mjs
@@ -0,0 +1,121 @@
+import { describe } from '../runner/riteway.mjs';
+import {
+  chip,
+  inputFlow,
+  inputData,
+  outputFlow,
+  outputData,
+  wire,
+  event,
+  externalGet,
+} from '../../core/index.mjs';
+import { js, compileAndRun, compileAndRunResult } from '../utils.mjs';
+import { Log } from '../lib.mjs';
+
+describe('[programs/events] events binding', async (assert) => {
+  const BIND_TARGET = { value: undefined };
+
+  const GetTarget = externalGet({ BIND_TARGET });
+
+  const Bind = chip('test/program/events/Bind', () => {
+    const exec = inputFlow('exec', () => {
+      target().value.addEventListener(event(), listener());
+      then();
+    });
+    const target = inputData('target');
+    const event = inputData('event', true);
+    const listener = inputData('listener');
+    const then = outputFlow('then');
+  });
+
+  const Listener = event('TestEvent', ['event']);
+
+  assert({
+    given: 'a bound event',
+    should: 'trigger',
+    actual: compileAndRun(
+      () => {
+        const exec = inputFlow('exec');
+
+        const bind = new Bind('test-event');
+        const target = new GetTarget();
+        wire(target.out.value, bind.in.target);
+        const listener = new Listener();
+        wire(listener.out.handle, bind.in.listener);
+
+        wire(exec, bind.in.exec);
+
+        const then = outputFlow('then');
+        const output = outputData('output');
+        wire(listener.out.then, then);
+        wire(listener.out.event, output);
+      },
+      (chip) => {
+        const res = [];
+        const t = (BIND_TARGET.value = new EventTarget());
+        t.dispatchEvent(new Event('test-event'));
+        chip.out.then(() => {
+          res.push(chip.out.output().type);
+        });
+        chip.in.exec();
+        t.dispatchEvent(new Event('test-event'));
+        t.dispatchEvent(new Event('test-event'));
+        return res;
+      },
+      null,
+      {
+        BIND_TARGET,
+      },
+    ),
+    expected: compileAndRunResult(
+      js`
+      class TestChip {
+        constructor() {
+          const $out = Object.seal({
+            output: undefined,
+            then: undefined
+          });
+
+          const TestEvent_1__handle = (...args) => {
+            $out.output = args[0];;
+            this.out.then();
+          };
+
+          Object.defineProperties(this.in = {}, {
+            exec: {
+              value: () => {
+                BIND_TARGET.value.addEventListener("test-event", TestEvent_1__handle);
+              }
+            }
+          });
+
+          Object.freeze(this.in);
+
+          Object.defineProperties(this.out = {}, {
+            output: {
+              value: () => $out.output
+            },
+
+            then: {
+              value: value => {
+                if (typeof value !== "undefined") {
+                  $out.then = value;
+                  return;
+                }
+
+                ($out.then || (() => {}))();
+              }
+            }
+          });
+
+          Object.freeze(this.out);
+
+          Object.defineProperty(this, "destroy", {
+            value: () => {}
+          });
+        }
+      }`,
+      ['test-event', 'test-event'],
+    ),
+  });
+});
