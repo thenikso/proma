fb79aa6c2f7ee1f1509850d448b8ce027dacf21c
Nicola Peduzzi <thenikso@gmail.com>
2021-03-04 17:07:53 +0100

    Add html/BindEvent

--- packages/proma-core/core/lib/html/BindEvent.mjs
+++ packages/proma-core/core/lib/html/BindEvent.mjs
@@ -8,18 +8,16 @@ export default function install({
   return registry.add(
     chip('lib/html/BindEvent', () => {
       const bind = inputFlow('bind', () => {
-        const t = document.getElementById(target());
-        t.addEventListener(type(), event());
+        target().addEventListener(type(), event());
         then();
       });
       const unbind = inputFlow('unbind', () => {
-        const t = document.getElementById(target());
-        t.removeEventListener(type(), event());
+        target().removeEventListener(type(), event());
         then();
       });
       const target = inputData('target', { canonical: true, type: 'Element' });
       const type = inputData('type', { canonical: true, type: 'String' });
-      const event = inputData('event', { type: '(Event) => void' });
+      const event = inputData('event', { type: '(event:Event) => void' });
 
       const then = outputFlow('then');
     }),
