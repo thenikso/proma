23b7bc3fa6fac8c1734e4f87e7c06943fa476f39
Nicola Peduzzi <thenikso@gmail.com>
2021-03-07 17:04:05 +0100

    GetProperty is flawless

--- packages/proma-core/core/lib/GetProperty.mjs
+++ packages/proma-core/core/lib/GetProperty.mjs
@@ -8,18 +8,13 @@ export default function install({
 }) {
   return registry.add(
     chip('lib/GetProperty', () => {
-      const exec = inputFlow('exec', () => {
-        value(target()[property()]);
-        then();
-      });
       const target = inputData('target', { type: 'Object', canonical: true });
       const property = inputData('property', {
         type: 'String',
         canonical: true,
       });
 
-      const then = outputFlow('then');
-      const value = outputData('value');
+      const value = outputData('value', () => target()[property()]);
     }),
   );
 }
