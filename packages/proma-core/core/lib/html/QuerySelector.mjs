6c6afd2715b638ee97657cdd7e9fc70cdcea8ba3
Nicola Peduzzi <thenikso@gmail.com>
2021-03-09 13:55:10 +0100

    QuerySelector is now flowless

--- packages/proma-core/core/lib/html/QuerySelector.mjs
+++ packages/proma-core/core/lib/html/QuerySelector.mjs
@@ -8,18 +8,16 @@ export default function install({
 }) {
   return registry.add(
     chip('lib/html/QuerySelector', () => {
-      const exec = inputFlow('exec', () => {
-        element(target().querySelector(selector()));
-        then();
-      });
       const target = inputData('target', { canonical: true, type: 'Element' });
       const selector = inputData('selector', {
         canonical: true,
         type: 'String',
       });
 
-      const then = outputFlow('then');
-      const element = outputData('element', 'Element');
+      const element = outputData('element', {
+        type: 'Element',
+        compute: () => target().querySelector(selector()),
+      });
     }),
   );
 }
