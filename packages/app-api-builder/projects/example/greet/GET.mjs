671ff11b0aa20599002e0115012ef1b2d5898b5b
Nicola Peduzzi <thenikso@gmail.com>
2021-03-13 21:13:14 +0100

    Proof of concept instand deploy of chip JSON!

--- packages/app-api-builder/projects/example/greet/GET.mjs
+++
@@ -1,3 +0,0 @@
-export default async function greet(query) {
-  return { fromFunc: true, query };
-}
--- packages/app-api-builder/src/routes/run/[project]/[func].ts
+++ packages/app-api-builder/src/routes/run/[project]/[func].ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
+import * as proma from '@proma/core';
 
 export async function get(req: Request, res: Response): Promise<void> {
   const { project, func } = req.params;
@@ -9,18 +10,33 @@ export async function get(req: Request, res: Response): Promise<void> {
     'projects',
     project,
     func,
-    'GET.mjs',
+    'GET.json',
   );
   let code = await fs.promises.readFile(codePath, { encoding: 'utf-8' });
-  if (code.startsWith('export default')) {
-    code = code.substr('export default'.length);
-  }
-  const makeHandler = new Function('return (' + code + ')');
-  const handler = makeHandler();
+  code = JSON.parse(code);
+  const Handler = proma.fromJSON(proma.chip, code);
+  const handler = new Handler();
+
+  const result = defer();
+  handler.in.query = req.query;
+  handler.out.then(() => {
+    result.resolve(handler.out.result());
+  });
+  handler.in.exec();
 
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
 
-  res.end(JSON.stringify(await handler(req.query)));
+  res.end(JSON.stringify({ fromChip: await result.promise }));
+}
+
+function defer() {
+  let resolve: (value: unknown) => void;
+  let reject: (reason?: any) => void;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
 }
