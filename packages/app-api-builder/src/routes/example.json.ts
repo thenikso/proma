917f8ac6661721b7ae7d7f5b3604cd705c36f277
Nicola Peduzzi <thenikso@gmail.com>
2021-03-13 12:13:38 +0100

    Test AWS CDK programmatic deployment

--- packages/app-api-builder/src/routes/example.json.ts
+++ packages/app-api-builder/src/routes/example.json.ts
@@ -1,4 +1,9 @@
 import type { Request, Response } from 'express';
+import { App } from '@aws-cdk/core';
+import { ExampleStack } from '../lib/ExampleStack';
+
+import { SdkProvider } from 'aws-cdk/lib/api/aws-auth';
+import { CloudFormationDeployments } from 'aws-cdk/lib/api/cloudformation-deployments';
 
 export async function get(req: Request, res: Response): Promise<void> {
   res.writeHead(200, {
@@ -13,13 +18,27 @@ export async function get(req: Request, res: Response): Promise<void> {
 }
 
 export async function post(req: Request, res: Response): Promise<void> {
+  const app = new App();
+  const stack = new ExampleStack(app, 'PromaExampleLambdaApiStack', {});
+
+  const stackArtifact = app.synth().getStackByName(stack.stackName);
+
+  const sdkProvider = await SdkProvider.withAwsCliCompatibleDefaults({
+    profile: 'nikso-proma',
+  });
+  const cloudFormation = new CloudFormationDeployments({ sdkProvider });
+  const deployResult = await cloudFormation.deployStack({
+    stack: stackArtifact,
+  });
+
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
 
   res.end(
     JSON.stringify({
-      message: `Ok POST`,
+      stack: stack.stackName,
+      deployResult,
     }),
   );
 }
