e61aaae387186f8b42f1cfad233a7556459c93ec
Nicola Peduzzi <thenikso@gmail.com>
2021-03-27 21:25:29 +0100

    Add chips

--- packages/proma-web-app/src/lib/PromaFileEditor.svelte
+++ packages/proma-web-app/src/lib/PromaFileEditor.svelte
@@ -1,7 +1,7 @@
 <script>
   // TODO use compiled library
   import * as proma from '@proma/core';
-  import { ChipBoardView } from '@proma/svelte-components';
+  import { ChipBoardView, Overlay } from '@proma/svelte-components';
 
   export let source;
   export let getEditedSource = undefined;
@@ -28,6 +28,94 @@
       return JSON.stringify(chipEditor.Chip.toJSON());
     };
   }
+
+  //
+  // Sub-chip request
+  //
+
+  let newSubChipRequest;
+  let registryList = proma.registry.list();
+
+  function registryListExcluding(chipToExclude) {
+    return registryList.filter((c) => c !== chipToExclude);
+  }
+
+  function newEventChipFromType(functionType) {
+    const ports = functionType.argumentsTypes.map((t, i) => ({
+      name: t.label || `arg${i + 1}`,
+      type: t.signature,
+    }));
+    const CustomEventChip = proma.event('CustomEvent', ...ports);
+    return new CustomEventChip();
+  }
+
+  //
+  // Event handler
+  //
+
+  function handleChipRequest(e) {
+    newSubChipRequest = e.detail;
+  }
 </script>
 
-<ChipBoardView chip={sourceChip} edit={chipEditor} />
+<ChipBoardView
+  id="PromaFile"
+  chip={sourceChip}
+  edit={chipEditor}
+  on:subChip:request={handleChipRequest}
+/>
+
+{#if newSubChipRequest}
+  <Overlay
+    anchor={{
+      x: newSubChipRequest.clientX - 5,
+      y: newSubChipRequest.clientY - 5,
+    }}
+    on:dismiss={() => (newSubChipRequest = null)}
+  >
+    <div>
+      {#if newSubChipRequest.fromType && newSubChipRequest.fromType.definitionKind === 'function'}
+        <button
+          type="button"
+          on:click={() => {
+            newSubChipRequest.provideChipInstance(
+              newEventChipFromType(newSubChipRequest.fromType),
+              // TODO also send connection hint
+            );
+            newSubChipRequest = null;
+          }}
+        >
+          Create custom event
+        </button>
+      {/if}
+      <div><b>Context chips</b></div>
+      {#each Object.values(newSubChipRequest.chip.customChipClasses) as chipClass (chipClass.URI)}
+        <div>
+          <button
+            type="button"
+            on:click={() => {
+              newSubChipRequest.provideChipInstance(new chipClass());
+              newSubChipRequest = null;
+            }}
+          >
+            {chipClass.URI}
+          </button>
+        </div>
+      {/each}
+      <div><b>All chips</b></div>
+      {#each registryListExcluding(newSubChipRequest.chip) as chipClass (chipClass.URI)}
+        <div>
+          <button
+            type="button"
+            on:click={() => {
+              newSubChipRequest.provideChipInstance(new chipClass());
+              newSubChipRequest = null;
+            }}
+          >
+            {chipClass.URI}
+          </button>
+        </div>
+      {/each}
+    </div>
+  </Overlay>
+{/if}
--- packages/proma-web-app/src/routes/edit/[hostId]/[projectSlug]/index.svelte
+++ packages/proma-web-app/src/routes/edit/[hostId]/[projectSlug]/index.svelte
@@ -36,13 +36,78 @@
   let getFileEditedSource;
 </script>
 
-{#if typeof window !== 'undefined'}
-  {#if selectedFileType === 'proma'}
-    <PromaFileEditor
-      source={selectedFileSource}
-      bind:getEditedSource={getFileEditedSource}
-    />
-  {:else}
-    <div>Unsupported file type "${selectedFileType}"</div>
+<div class="Editor Editor-fileType-{selectedFileType}">
+  {#if typeof window !== 'undefined'}
+    {#if selectedFileType === 'proma'}
+      <PromaFileEditor
+        source={selectedFileSource}
+        bind:getEditedSource={getFileEditedSource}
+      />
+    {:else}
+      <div>Unsupported file type "${selectedFileType}"</div>
+    {/if}
   {/if}
-{/if}
+
+  <div class="EditorHeader">
+    <div class="Breadcrumbs">
+      <div class="current">Title</div>
+    </div>
+    <div class="Spacer" />
+    {#if $$slots.tools}
+      <div class="Tools">
+        <slot name="tools" />
+      </div>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .Editor {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  /* Header */
+
+  .EditorHeader {
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    padding: 30px;
+
+    display: flex;
+    align-items: center;
+
+    transition: background-color 0.25s ease;
+
+    pointer-events: none;
+  }
+
+  .EditorHeader:hover {
+    transition: background-color 1s ease;
+    transition-delay: 0.5s;
+    background-color: rgba(255, 255, 255, 0.7);
+  }
+
+  .EditorHeader .Breadcrumbs {
+    pointer-events: all;
+  }
+
+  .EditorHeader .Breadcrumbs .current {
+    font-size: 1.5em;
+    font-weight: 500;
+    color: #2e3741;
+  }
+
+  .EditorHeader .Spacer {
+    flex-grow: 1;
+  }
+
+  .EditorHeader .Tools {
+    pointer-events: all;
+  }
+</style>
