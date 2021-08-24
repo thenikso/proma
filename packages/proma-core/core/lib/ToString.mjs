e33e219017ca7a50337779fe558a903e9f4e98f1
Nicola Peduzzi <thenikso@gmail.com>
2021-08-24 10:56:40 +0200

    Add type to ToString

--- packages/proma-core/core/lib/ToString.mjs
+++ packages/proma-core/core/lib/ToString.mjs
@@ -2,7 +2,10 @@ export default function install({ registry, chip, inputData, outputData }) {
   return registry.add(
     chip('lib/ToString', () => {
       const target = inputData('target', { canonical: true });
-      const string = outputData('string', () => String(target()));
+      const string = outputData('string', {
+        compute: () => String(target()),
+        type: 'string',
+      });
     }),
   );
 }
